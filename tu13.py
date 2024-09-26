63b1a5f485692fcb404bad58837a855ea9626bef
ridhimagarodia <205397@gardenschool.edu.my>
2024-09-26 14:09:06 +0800

    Add files via upload

---
+++ tu13.py
@@ -0,0 +1,11 @@
+#Excercise: 1
+myList = [1,2,3,4,5,6,7,8,9,10]
+print(sum(myList))
+
+#Excercise: 2
+myList = [23,54,65,12,34,87,21,4,91,32,39]
+print("The largest number in the list is: ", max(myList))
+print("The smallest number in the list is: ", min(myList))
+print("The sum of the list is: ", sum(myList))
+print("The length of the list is: ", len(myList))
+print("The average of the list is: ", sum(myList)/len(myList))
